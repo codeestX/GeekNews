b5da2ac6df827a9ef5ff8f8a6d093cd6ac19fa7e
Est <codeest.dev@gmail.com>
2016-08-18 21:42:37 +0800

    add comment layout

--- app/src/main/java/com/codeest/geeknews/util/DateUtil.java
+++ app/src/main/java/com/codeest/geeknews/util/DateUtil.java
@@ -1,5 +1,6 @@
 package com.codeest.geeknews.util;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -44,5 +45,79 @@ public class DateUtil {
     public static int getCurrentDay() {
         Calendar cal=Calendar.getInstance();
         return cal.get(Calendar.DATE);
+    }
+
+    /**
+     * 将时间戳转化为字符串
+     * @param showTime
+     * @return
+     */
+    public static String formatTime2String(long showTime) {
+        return formatTime2String(showTime,false);
+    }
+
+    public static String formatTime2String(long showTime , boolean haveYear) {
+        String str = "";
+        long distance = System.currentTimeMillis()/1000 - showTime;
+        if(distance < 300){
+            str = "刚刚";
+        }else if(distance >= 300 && distance < 600){
+            str = "5分钟前";
+        }else if(distance >= 600 && distance < 1200){
+            str = "10分钟前";
+        }else if(distance >= 1200 && distance < 1800){
+            str = "20分钟前";
+        }else if(distance >= 1800 && distance < 2700){
+            str = "半小时前";
+        }else if(distance >= 2700){
+            Date date = new Date(showTime * 1000);
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+            str = formatDateTime(sdf.format(date) , haveYear);
+        }
+        return str;
+
+    }
+
+    public static String formatDateTime(String time ,boolean haveYear) {
+        SimpleDateFormat format = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm");
+        if(time == null){
+            return "";
+        }
+        Date date = null;
+        try {
+            date = format.parse(time);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        Calendar current = Calendar.getInstance();
+        Calendar today = Calendar.getInstance();
+        today.set(Calendar.YEAR, current.get(Calendar.YEAR));
+        today.set(Calendar.MONTH, current.get(Calendar.MONTH));
+        today.set(Calendar.DAY_OF_MONTH, current.get(Calendar.DAY_OF_MONTH));
+        today.set(Calendar.HOUR_OF_DAY, 0);
+        today.set(Calendar.MINUTE, 0);
+        today.set(Calendar.SECOND, 0);
+        Calendar yesterday = Calendar.getInstance();
+        yesterday.set(Calendar.YEAR, current.get(Calendar.YEAR));
+        yesterday.set(Calendar.MONTH, current.get(Calendar.MONTH));
+        yesterday.set(Calendar.DAY_OF_MONTH, current.get(Calendar.DAY_OF_MONTH)-1);
+        yesterday.set(Calendar.HOUR_OF_DAY, 0);
+        yesterday.set(Calendar.MINUTE, 0);
+        yesterday.set(Calendar.SECOND, 0);
+
+        current.setTime(date);
+        if(current.after(today)){
+            return "今天 "+time.split(" ")[1];
+        }else if(current.before(today) && current.after(yesterday)){
+            return "昨天 "+time.split(" ")[1];
+        }else{
+            if(haveYear) {
+                return time;
+            }else {
+                int index = time.indexOf("-")+1;
+                return time.substring(index,time.length());
+            }
+        }
     }
 }
